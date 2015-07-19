3940a354bab84b3caf9703ba506cfe79370db3a8
Markus Schmall <schmall.markus@gmail.com>
2015-07-19 17:46:25 +0200

    fixed wrong path names

--- requests/sendalarm.sh
+++ requests/sendalarm.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./alarm.xml http://localhost:8080/honeyalarmg2/reportalarm
+curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./alarm.xml http://localhost:8080/reportalarm
--- requests/sendupd.sh
+++ requests/sendupd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./update.xml http://localhost:8080/honeyalarmg2/updateip
+curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./update.xml http://localhost:8080/updateip
