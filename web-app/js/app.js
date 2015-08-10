293792cbf06f4715bb48336071e36b8f6e425ab2
Markus Schmall <schmall.markus@gmail.com>
2015-08-10 21:04:24 +0200

    added some documentation

--- web-app/js/app.js
+++ web-app/js/app.js
@@ -1,13 +1,21 @@
 var app = angular.module("honeyalarm", []);
 
+//
+// Directly retrieve the JSON data from the REST Interface via the class Name UIReport
+//
+
 app.controller("ReportController", function($scope, $http) {
     $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
     $http.get('/UIReport.json').
         success(function(data, status, headers, config) {
-            $scope.reports = data;
+            $scope.reportsAngular = data;
         });
 });
 
+
+//
+// Directly retrieve the JSON data from the REST Interface via the class Name Report
+//
 
 app.controller("AlarmController", function($scope, $http) {
     $http.defaults.headers.common["X-Custom-Header"] = "Angular.js";
