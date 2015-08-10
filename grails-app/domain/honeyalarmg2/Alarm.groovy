6bba4d98a6d9ffb6c329588fd4f5e1ce3d5d0bcd
Markus Schmall <schmall.markus@gmail.com>
2015-08-10 21:23:30 +0200

    added basic graph controller demo code

---
+++ grails-app/domain/honeyalarmg2/Alarm.groovy
@@ -0,0 +1,14 @@
+package honeyalarmg2
+
+class Alarm
+{
+
+    String time
+    String type
+    String request
+    String status
+    String attacker;
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/org/metams/honeyalarm/SecRole.groovy
@@ -0,0 +1,36 @@
+package org.metams.honeyalarm
+
+class SecRole implements Serializable {
+
+	private static final long serialVersionUID = 1
+
+	String authority
+
+	SecRole(String authority) {
+		this()
+		this.authority = authority
+	}
+
+	@Override
+	int hashCode() {
+		authority?.hashCode() ?: 0
+	}
+
+	@Override
+	boolean equals(other) {
+		is(other) || (other instanceof SecRole && other.authority == authority)
+	}
+
+	@Override
+	String toString() {
+		authority
+	}
+
+	static constraints = {
+		authority blank: false, unique: true
+	}
+
+	static mapping = {
+		cache true
+	}
+}
