8cf780ae304cc85a7dc68f75fe440f1796c064a7
Markus Schmall <schmall.markus@gmail.com>
2015-08-10 21:21:36 +0200

    removed spring security

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -62,7 +62,7 @@ grails.project.dependency.resolution = {
         compile ':cache:1.1.8'
         compile ":asset-pipeline:2.1.5"
         compile ":quartz:1.0.1"
-        compile ":spring-security-core:2.0-RC5"
+//        compile ":spring-security-core:2.0-RC5"
 
 
         // plugins needed at runtime but not for compilation
