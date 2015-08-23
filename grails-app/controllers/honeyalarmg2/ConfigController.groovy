4219bc4848f0af68dcabd190ab9fd69e2d2e4141
Markus Schmall <schmall.markus@gmail.com>
2015-08-23 17:42:24 +0200

    added basic handling for Telegram

--- grails-app/controllers/honeyalarmg2/ConfigController.groovy
+++ grails-app/controllers/honeyalarmg2/ConfigController.groovy
@@ -3,10 +3,46 @@ package honeyalarmg2
 class ConfigController
 {
 
+    def saveConfig()
+    {
+
+        ConfigHG x = new ConfigHG(params)
+        x.lastChanged = (String)new Date()
+        x.changedFromIP = request.getRemoteAddr()
+
+        // find old database entry
+        ConfigHG update = ConfigHG.findByNameMandant(x.nameMandant)
+        update.delete()
+
+        x.save(flush: true)
+
+        redirect(controller: "IndexAngular", action: "index")
+    }
+
     def index()
     {
         ConfigHG config = ConfigHG.findByNameMandant("default")
         def size = ConfigHG.all.size()
-        [config: config]
+        def telegramID = []
+        def telegramFirstName = []
+        def telegramLastName = []
+
+
+        if (config == null)
+        {
+            println "bummer, config not found!!!"
+        }
+
+        if (config.useTelegram != null && config.useTelegram)
+        {
+            TelegramController x = new TelegramController()
+            (telegramID, telegramFirstName, telegramLastName) = x.getChatIDBasis(config.telegramToken)
+        }
+        else
+        {
+            println "no telegram"
+        }
+
+        [config: config, size:size, telegramID:telegramID, telegramFirstName:telegramFirstName, telegramLastName:telegramLastName, ip: request.getRemoteAddr()  ]
     }
 }
