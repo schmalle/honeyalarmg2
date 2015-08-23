83067e7da40ffdd2269241adb711ae8d0bff10c5
Markus Schmall <schmall.markus@gmail.com>
2015-08-23 18:24:56 +0200

    fixed typos

--- requests/sendupd.sh
+++ requests/sendupd.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
 
-curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./update.xml http://localhost:8080/updateip
+curl -X POST --header "Content-Type:text/xml;charset=UTF-8" -d @./update.xml http://127.0.0.1:8080/updateip
--- requests/telegram/getme.sh
+++ requests/telegram/getme.sh
@@ -1,4 +1,8 @@
 #!/usr/bin/env bash
 
+echo "Get my own data:"
 curl -s -X POST https://api.telegram.org/bot$1/getMe
+echo ""
+echo "Get my updates"
 curl -s -X POST https://api.telegram.org/bot$1/getUpdates
+echo ""
