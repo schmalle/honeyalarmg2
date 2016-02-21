7376ac8fe28c73f385eacbfc69b3e84faf648898
Markus Schmall <schmall.markus@gmail.com>
2016-02-21 16:42:08 +0100

    cleanup code

--- grails-app/domain/honeyalarmg2/ConfigHG.groovy
+++ grails-app/domain/honeyalarmg2/ConfigHG.groovy
@@ -2,29 +2,25 @@ package honeyalarmg2
 
 class ConfigHG  {
 
-    String nameMandant
     String useImage
     String image
     String changedFromIP
     String added
     String lastChanged
-    String useTelegram
-    String telegramToken
+
     String useTwitter
     String twitterToken
     String twitterOAuthConsumerKey
     String twitterOAuthConsumerSecret
     String twitterOAuthAccessToken
     String twitterOAuthAccessTokenSecret
-    String usePushover
-    String pushoverToken
 
+
+    String useSicherheitstacho
     String userNameTSecRadar
     String passwordTSecRadar
     String serverTSecRadar
 
-
-    LinkedList telegramUsers
 
     static constraints = {
 
