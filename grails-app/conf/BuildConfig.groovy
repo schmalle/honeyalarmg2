198c9bbbe37b26395dc192bc55af7ee18ffa254d
Markus Schmall <schmall.markus@gmail.com>
2016-03-06 20:39:17 +0100

    added fix for tomcat

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -7,6 +7,12 @@ grails.project.target.level = 1.6
 grails.project.source.level = 1.6
 //grails.project.war.file = "target/${appName}-${appVersion}.war"
 
+grails.war.resources = { stagingDir, args ->
+    copy(todir: "${stagingDir}/WEB-INF/lib", flatten: "true") {
+        fileset(dir: "${grailsHome}/lib", includes: "**/jline-*.jar, **/jansi-*.jar")
+    }
+}
+
 grails.project.fork = [
     // configure settings for compilation JVM, note that if you alter the Groovy version forked compilation is required
     //  compile: [maxMemory: 256, minMemory: 64, debug: false, maxPerm: 256, daemon:true],
